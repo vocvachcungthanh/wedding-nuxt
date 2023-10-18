af1c357e3ecbfb141ca35b2d801a9125b6fe264e
Thành Nguyễn Hữu <thanhnguyenhuu@Thanhs-MacBook-Pro.local>
2023-10-18 08:23:34 +0700

    style: loi chuc, logo

--- components/guestkbook/FooterGuestkbook.vue
+++ components/guestkbook/FooterGuestkbook.vue
@@ -1,11 +1,15 @@
 <template>
   <div class="group__footer">
-    <h3 class="ceo__title">CEO của Biihappy</h3>
-
-    <p class="ceo__info">
-      "Một cuộc hôn nhân thành công đòi hỏi phải yêu nhiều lần, và luôn ở cùng
-      một người" - Chúc cho hai bạn sẽ có được một cuộc hôn nhân viên mãn, trăm
-      năm hạnh phúc!
-    </p>
+    <ItemGuestkbook />
   </div>
 </template>
+
+<script>
+import ItemGuestkbook from './ItemGuestkbook.vue'
+
+export default {
+  components: {
+    ItemGuestkbook,
+  },
+}
+</script>
---
+++ components/guestkbook/ItemGuestkbook.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="guestkbook__box">
+    <div class="guestkbook__item">
+      <h3 class="ceo__title">CEO của Biihappy</h3>
+
+      <p class="ceo__info">
+        "Một cuộc hôn nhân thành công đòi hỏi phải yêu nhiều lần, và luôn ở cùng
+        một người" - Chúc cho hai bạn sẽ có được một cuộc hôn nhân viên mãn,
+        trăm năm hạnh phúc!
+      </p>
+    </div>
+    <div class="guestkbook__item">
+      <h3 class="ceo__title">CEO của Biihappy</h3>
+
+      <p class="ceo__info">
+        "Một cuộc hôn nhân thành công đòi hỏi phải yêu nhiều lần, và luôn ở cùng
+        một người" - Chúc cho hai bạn sẽ có được một cuộc hôn nhân viên mãn,
+        trăm năm hạnh phúc!
+      </p>
+    </div>
+    <div class="guestkbook__item">
+      <h3 class="ceo__title">CEO của Biihappy</h3>
+
+      <p class="ceo__info">
+        "Một cuộc hôn nhân thành công đòi hỏi phải yêu nhiều lần, và luôn ở cùng
+        một người" - Chúc cho hai bạn sẽ có được một cuộc hôn nhân viên mãn,
+        trăm năm hạnh phúc!
+      </p>
+    </div>
+    <div class="guestkbook__item">
+      <h3 class="ceo__title">CEO của Biihappy</h3>
+
+      <p class="ceo__info">
+        "Một cuộc hôn nhân thành công đòi hỏi phải yêu nhiều lần, và luôn ở cùng
+        một người" - Chúc cho hai bạn sẽ có được một cuộc hôn nhân viên mãn,
+        trăm năm hạnh phúc!
+      </p>
+    </div>
+  </div>
+</template>
